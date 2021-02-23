5dd156daeb93274b82948795ad348703bef9d0cd
Matt <mattcharlesh@gmail.com>
2021-02-22 23:01:59 -0500

    adding Resolvers

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+//import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
 import express from 'express'
+import {ApolloServer} from 'apollo-server-express'
+import {buildSchema} from 'type-graphql'
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
  
   const app = express();
-  app.get('/', (_, res) => {
-    res.send('hello')
+
+  const apolloServer = new ApolloServer({
+      schema: await buildSchema({
+          resolvers: [],
+          validate: false
+      })
   })
+  
   app.listen(4000, ()=> {
       console.log('server started on localhost:4000')
   })
